120520ddec6340b639f128441e07594f99373003
bkrraj <bkrraj@gmail.com>
2023-04-27 11:17:21 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-ro-cotse0eg3ytd.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'apache123',
+    DB_DATABASE : 'webappdb'
+});
